83418c337c555b4756e355dc5bb6d8d39ea0041d
trinistr <trinistr@gmail.com>
2017-09-30 16:33:56 +0300

    Scripts now use binary in the top directory and announce running compiled programs

--- scripts/bfcompile.bat
+++ scripts/bfcompile.bat
@@ -1,7 +1,9 @@
-../src/bfcompile %1.bf -w4 -z -v -t windows
+../bfcompile %1.bf -w4 -z -v -t windows
 nasm -f win32 %1.asm
 del %1.asm
 gcc %1.obj -o %1
 del %1.obj
+echo
+echo Running %1:
 %1
 
--- scripts/bfcompile.sh
+++ scripts/bfcompile.sh
@@ -6,9 +6,11 @@ if [ ! -f $1 -o -z $1 ]; then
 fi
 
 name=$(basename $1 .bf)
-../src/bfcompile "$1" -w 4 -z -v
+../bfcompile "$1" -w 4 -z -v
 nasm -f elf32  "$name.asm"
 rm "$name.asm"
 gcc -m32 "$name.o" -o "$name"
 rm "$name.o"
+echo
+echo "Running $name:"
 ./$name
